7a079efbff8c13390bfe918bf85bc9ad6bb8a905
sproduce <5172543@gmail.com>
2022-08-30 01:33:02 +0300

    bux fix

--- resources/views/rentEvent/eventRental/extendedAdd.blade.php
+++ resources/views/rentEvent/eventRental/extendedAdd.blade.php
@@ -8,10 +8,8 @@
 
 @section('content')
 
-<form method="POST" action="/eventRental">
+<form method="POST" action="/rentEvent/{{$eventObj->id}}">
     @csrf
-    <input name="eventId" value="{{$eventObj->id}}" hidden/>
-    <input name="duration" value="{{$eventObj->duration}}" hidden/>
     <div class="form-row text-center">
         <div class="form-group col-md-3 input-group-sm">
             <label for="contractText" title="Автомобиль">Машина</label>
